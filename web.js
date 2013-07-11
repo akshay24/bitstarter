0ef3e6e3d67388a1d380132754431fa54ba2daa6
akshay24 <ubuntu@ip-172-31-40-3.us-west-2.compute.internal>
2013-07-11 18:37:16 +0000

    Fixed fs

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
+    var fs = require('fs'); 
     var text = fs.readFileSync('index.html');
     console.log(text.toString('utf8'));
     var ch = text.toString('utf8');
